2689563af3b4871277ebb4643909711b918b4285
Ruben Htun <rubenhtun2003@gmail.com>
2025-11-05 10:26:35 +0700

    boilerplate-project-filemetadata

---
+++ boilerplate-project-filemetadata/index.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+
+var app = express();
+var upload = multer({ dest: "uploads/" });
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  const fileInfo = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
+
+  res.json(fileInfo);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
