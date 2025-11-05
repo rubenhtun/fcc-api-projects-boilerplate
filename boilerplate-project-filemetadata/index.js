98674e2f68e96ec6065cb16849ac8cb38323be01
Ruben Htun <rubenhtun2003@gmail.com>
2025-11-05 11:18:49 +0700

    refactor: use __dirname for more reliable file paths

--- boilerplate-project-filemetadata/index.js
+++ boilerplate-project-filemetadata/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
